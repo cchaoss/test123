ef549a4aaee8c41c2e7c07224f6583d9ee7a321d
wudong <862281335@qq.com>
2017-06-08 03:24:23 +0000

    移植到nanopi，使用终端输出

--- main.c
+++ main.c
@@ -11,23 +11,24 @@
 #define Serial_Name "/dev/ttyUSB0"
 
 
-uint8_t buf[6] = {0xAA,0,0,0,0,0};
+uint8_t buf[6] = {0x0B,1,2,3,4,0};
 
 int main() {
 
-	int fd = 0;
+//	int fd = 0;
         
-//       for(unsigned char i = 0;i<100;i++) {
-//		printf("count:%d\n",i);
-//		usleep(100000);
-//	}
-
+/*       for(unsigned char i = 0;i<100;i++) {
+		printf("count:%d\n",i);
+		usleep(100000);
+	}
+*/
+/*
 	fd = serial_open(fd, Serial_Name);
 	if(fd == false) 
 		printf("open serial failed!Return fd=:%d\n", fd);
 	else if(serial_init(fd) == false) 
 		printf("serial initialize failed\n");
-	
+*/
 	opticflow_module_init();
 	opticflow_module_start();
 
@@ -36,8 +37,8 @@ int main() {
 			memcpy(&buf[1],&opticflow_result.flow_x,2);
 			memcpy(&buf[3],&opticflow_result.flow_y,2);
 			buf[5] = (buf[1]^buf[2]^buf[3]^buf[4])&0xFF;
-			//printf(" buf[1]:%d\n,buf[2]:%d\n,buf[3]:%d\n\n\n\n,buf[4]:%d\n",buf[1],buf[2],buf[3],buf[4]);		
-			write(fd, buf, 6);
+			write(1, buf, 6);
+			//printf("%d\n",buf[5]);
 		}
 
 		usleep(10000);//100hz
@@ -46,7 +47,3 @@ int main() {
 	opticflow_module_stop();
 	return 0;
 }
-
-
-
-
